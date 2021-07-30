9b4fd9350e2bac714812530ce2c10650df7bbecd
eric <eric@hamelyn.com>
2021-07-30 15:55:07 +0200

    Servidor simplificado

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,32 @@
-import createError from "http-errors";
 import express, { Request, Response, NextFunction } from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404
 app.use(function (req, res, next) {
-  next(createError(404));
+  res.status(404).send({ message: "Endpoint not found" });
 });
 
 // error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.error("500 Internal server error:", err);
+  res.status(500).send({ message: "Internal server error" });
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+const PORT = process.env.PORT;
+const port = PORT ? parseInt(PORT) : 3000;
+app.set("port", port);
+
+app.listen(port, () => {
+  console.info("Listening on port", port);
 });
 
 export default app;
