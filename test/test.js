db0e642af5ed162752ef9a1a69e3489446408105
eric <eric@hamelyn.com>
2021-07-30 09:24:22 +0200

    Typescript added

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../dist/app');
 
 describe('App', function() {
   it('has the default page', function(done) {
