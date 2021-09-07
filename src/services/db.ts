9819e62f5a1e9226ac49aa6777b973e683ac52a9
eric <eric@hamelyn.com>
2021-09-07 17:48:51 +0200

    DB initialization (db and tables creation, no population)

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,30 +1,66 @@
 // get the client
-import mysql from "mysql2/promise";
+import { readFileSync } from "fs";
+import mysql, {
+  OkPacket,
+  ResultSetHeader,
+  RowDataPacket,
+} from "mysql2/promise";
+import path from "path";
 import { getLogger } from "./winston";
+
+type Rows =
+  | RowDataPacket[]
+  | RowDataPacket[][]
+  | OkPacket
+  | OkPacket[]
+  | ResultSetHeader;
+
+interface QueryResult<T extends Rows> {
+  rows: T;
+  fields: string[];
+}
 
 let connection: mysql.Connection;
 
 const logger = getLogger("api");
+
+const DB_NAME = "kitchen_js";
 
 export async function createConnection(): Promise<void> {
   logger.info("Connecting to MySQL ...");
   connection = await mysql.createConnection({
     host: "localhost",
     user: "root",
-    database: "test",
+    password: "root",
+    multipleStatements: true,
   });
   logger.info("Connected to MySQL");
+
+  const { rows: databases } = await query<RowDataPacket[]>("show databases;");
+  const database = databases.find((row) => row.Database === DB_NAME);
+  if (database) {
+    logger.info("Database found. Using existing database");
+    await query<OkPacket>(`use ${DB_NAME};`);
+  } else {
+    logger.info("Database not found. Creating database...");
+    await query<OkPacket>(`create database ${DB_NAME};`);
+    await query<OkPacket>(`use ${DB_NAME};`);
+    await query<OkPacket[]>(readSqlScript("create-tables"));
+    logger.info("Database created...");
+  }
 }
 
-export async function testQuery({
-  name,
-  age,
-}: {
-  name: string;
-  age: number;
-}): Promise<void> {
-  const [rows, fields] = await connection.execute(
-    "SELECT * FROM `table` WHERE `name` = ? AND `age` > ?",
-    [name, age]
-  );
+export function closeConnection(): void {
+  logger.info("Disconnecting from MySQL ...");
+  connection.destroy();
 }
+
+async function query<T extends Rows>(query: string): Promise<QueryResult<T>> {
+  const [rows, fields] = await connection.query<T>(query);
+  const fieldNames = fields && fields.filter((f) => f).map((f) => f.name);
+  logger.debug("MySQL: > " + query, { rows, fields: fieldNames });
+  return { rows, fields: fieldNames || [] };
+}
+
+const readSqlScript = (filename: string) =>
+  readFileSync(path.resolve(__dirname, `../../sql/${filename}.sql`)).toString();
