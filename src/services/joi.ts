b05511d9f7cc9d63bfe744d720ebcbd9cda4ed93
eric <eric@hamelyn.com>
2021-10-01 17:28:32 +0200

    fixes and logs

--- src/services/joi.ts
+++ src/services/joi.ts
@@ -1,14 +1,20 @@
 import { RequestHandler } from "express";
 import Joi from "joi";
 
+import { getLogger } from "@services/winston";
+
+const logger = getLogger();
+
 const validate =
   (fieldToValidate: "body" | "query" | "params") =>
   (schema: Joi.Schema): RequestHandler =>
   (req, res, next) => {
     const { value, error } = schema.validate(req[fieldToValidate]);
     if (error) {
+      logger.error(`Validation of ${fieldToValidate} failed:`, error);
       res.status(400).send({ message: error.message });
     } else {
+      logger.debug(`Validation passed. Request ${fieldToValidate}:`, value);
       req[fieldToValidate] = value;
       next();
     }
@@ -18,7 +24,7 @@ export const validateBody = validate("body");
 export const validateQuery = validate("query");
 export const validatePath = validate("params");
 
-export const validatePathId = validateBody(
+export const validatePathId = validatePath(
   Joi.object({
     id: Joi.number().integer().required(),
   })
