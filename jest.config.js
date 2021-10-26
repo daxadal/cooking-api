ebaa907e38c5ef5b004c1db9f9b62297d1d38ef3
eric <eric@hamelyn.com>
2021-10-26 15:28:52 +0200

    Configuration updated

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig.json");
+
+module.exports = {
+  roots: ["test"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|js)"],
+  transform: {
+    "^.+\\.(ts|js)$": "ts-jest",
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  testTimeout: 10000,
+};
