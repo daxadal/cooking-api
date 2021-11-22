0a71b5b162e36017133f1ad0ce91ca5901c3a9b9
eric <eric@hamelyn.com>
2021-11-22 18:06:33 +0100

    app split into app and index

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import "module-alias/register";
 import express, { Request, Response, NextFunction } from "express";
 
 import indexRouter from "@routes/index";
@@ -7,29 +6,7 @@ import ingredientsRouter from "@routes/ingredients";
 import recipesRouter from "@routes/recipes";
 import stepsRouter from "@routes/steps";
 import utensilsRouter from "@routes/utensils";
-
-import { getLogger, initLogger } from "@services/winston";
-import { closeConnection, createConnection } from "@services/db/setup";
-import { configDebug } from "@config/index";
-
-const logger = getLogger();
-
-logger.info("=== SERVER STARTUP ===");
-
-if (configDebug.dotenv.error) logger.warn(`Could NOT parse .env, Error:`, configDebug.dotenv.error);
-else if (!configDebug.dotenv.parsed) logger.info(`Parsing .env produced no result`);
-else
-  logger.info(
-    `.env parsed. ${Object.keys(configDebug.dotenv.parsed).length} variables found.`
-  );
-
-if (configDebug.parsingErrors.length > 0) {
-  logger.error(`@config initialization failed`, { errors: configDebug.parsingErrors });
-  logger.info("Exiting on error...\n");
-  process.exit(1);
-}
-
-createConnection();
+import { initLogger } from "@services/winston";
 
 const app = express();
 
@@ -58,22 +35,4 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send({ message: "Internal server error" });
 });
 
-const PORT = process.env.PORT;
-const port = PORT ? parseInt(PORT) : 3000;
-app.set("port", port);
-
-const server = app.listen(port, () => {
-  logger.info(`Listening on port ${port}`);
-});
-
-process.on("SIGINT", () => {
-  closeConnection();
-  process.exit(0);
-});
-
-process.on("SIGTERM", () => {
-  closeConnection();
-  process.exit(0);
-});
-
-export default server;
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import "module-alias/register";
+
+import { getLogger } from "@services/winston";
+import { closeConnection, createConnection } from "@services/db/setup";
+import { configDebug } from "@config/index";
+import app from "./app";
+
+const logger = getLogger();
+
+logger.info("=== SERVER STARTUP ===");
+
+if (configDebug.dotenv.error)
+  logger.warn(`Could NOT parse .env, Error:`, configDebug.dotenv.error);
+else if (!configDebug.dotenv.parsed)
+  logger.info(`Parsing .env produced no result`);
+else
+  logger.info(
+    `.env parsed. ${
+      Object.keys(configDebug.dotenv.parsed).length
+    } variables found.`
+  );
+
+if (configDebug.parsingErrors.length > 0) {
+  logger.error(`@config initialization failed`, {
+    errors: configDebug.parsingErrors,
+  });
+  logger.info("Exiting on error...\n");
+  process.exit(1);
+}
+
+createConnection({});
+
+const PORT = process.env.PORT;
+const port = PORT ? parseInt(PORT) : 3000;
+app.set("port", port);
+
+const server = app.listen(port, () => {
+  logger.info(`Listening on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+  closeConnection();
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  closeConnection();
+  process.exit(0);
+});
+
+export default server;
