155904510e4db636961cd9660801d7ade76b49b7
eric <eric@hamelyn.com>
2022-01-04 17:16:15 +0100

    CI/CD configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,25 @@
 module.exports = async () => {
   process.env.DB_NAME = "jest_" + new Date().toISOString().replace(/\W/g, "_");
+
+  process.env.WINSTON_CONSOLE_LEVEL = "none";
+
+  process.env.WINSTON_SLACK_LEVEL = "none";
+  process.env.WINSTON_SLACK_PRIORITY_WEBHOOK = undefined;
+  process.env.WINSTON_SLACK_NON_PRIORITY_WEBHOOK = undefined;
+
+  if (process.env.CI) {
+    process.env.ENV = "CI_JEST";
+
+    process.env.DB_HOST = "localhost";
+
+    process.env.WINSTON_FILE_LEVEL = "info";
+    process.env.WINSTON_FILE_PREFIX = "__jest__";
+  } else {
+    process.env.ENV = "JEST";
+
+    process.env.DB_HOST = "mysql-docker";
+
+    process.env.WINSTON_FILE_LEVEL = "none";
+    process.env.WINSTON_FILE_PREFIX = "__jest__";
+  }
 };
