0bd04abff34dc91817a3eba08ecf2778bbeb3ce2
eric <eric@hamelyn.com>
2022-01-05 15:11:44 +0100

    Added Jest reports with jest-unit

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,24 @@
 const { pathsToModuleNameMapper } = require("ts-jest/utils");
 const { compilerOptions } = require("./tsconfig.json");
 
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: "<rootDir>/",
+});
 module.exports = {
+  globalSetup: "<rootDir>/jest.setup.js",
+  globalTeardown: "<rootDir>/jest.teardown.js",
+
+  reporters: ["default", "jest-junit"],
+
   roots: ["test"],
   testMatch: ["**/?(*.)+(spec|test).+(ts|js)"],
   transform: {
     "^.+\\.(ts|js)$": "ts-jest",
   },
-  globalSetup: "<rootDir>/jest.setup.js",
-  globalTeardown: "<rootDir>/jest.teardown.js",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/",
-  }),
+
+  moduleNameMapper: {
+    ...moduleNameMapper,
+    "^test/(.*)$": "<rootDir>/test/$1",
+  },
   testTimeout: 10000,
 };
