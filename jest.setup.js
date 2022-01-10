5c539ad36bb92f57c8474daeb3a897015f2164a3
eric <eric@hamelyn.com>
2022-01-10 12:31:31 +0100

    swap DB_HOST for jest tests (local and CI)

--- jest.setup.js
+++ jest.setup.js
@@ -10,14 +10,14 @@ module.exports = async () => {
   if (process.env.CI) {
     process.env.ENV = "CI_JEST";
 
-    process.env.DB_HOST = "localhost";
+    process.env.DB_HOST = "mysql-docker";
 
     process.env.WINSTON_FILE_LEVEL = "info";
     process.env.WINSTON_FILE_PREFIX = "__jest__";
   } else {
     process.env.ENV = "JEST";
 
-    process.env.DB_HOST = "mysql-docker";
+    process.env.DB_HOST = "localhost";
 
     process.env.WINSTON_FILE_LEVEL = "none";
     process.env.WINSTON_FILE_PREFIX = "__jest__";
