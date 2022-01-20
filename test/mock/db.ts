f36475e428f5fc98a8c156459808d02ae74756a8
eric <eric@hamelyn.com>
2022-01-20 18:00:06 +0100

    Create mock methods

--- test/mock/db.ts
+++ test/mock/db.ts
@@ -1,6 +1,9 @@
 import { OkPacket } from "mysql2/promise";
 
 import { query, QueryResult } from "@services/db/setup";
+import { Ingredient } from "@services/db/ingredient";
+import { Utensil } from "@services/db/utensil";
+import { SimpleStep } from "@services/db/step";
 
 export function clearDatabase(): Promise<QueryResult<OkPacket>[]> {
   return Promise.all([
@@ -8,4 +11,40 @@ export function clearDatabase(): Promise<QueryResult<OkPacket>[]> {
     query<OkPacket>("delete from utensil;"),
     query<OkPacket>("delete from step;"),
   ]);
+}
+
+export async function createMockIngredient({
+  id,
+  name,
+  type,
+}: Ingredient): Promise<number> {
+  const { rows } = await query<OkPacket>(
+    "insert into ingredient (id, name, type) values (:id, :name, :type);",
+    { id, name, type }
+  );
+  return rows.insertId;
+}
+
+export async function createMockUtensil({
+  id,
+  name,
+  waitTimeInMillis,
+}: Utensil): Promise<number> {
+  const { rows } = await query<OkPacket>(
+    "insert into utensil (id, name, waitTimeInMillis) values (id, :name, :waitTimeInMillis);",
+    { id, name, waitTimeInMillis }
+  );
+  return rows.insertId;
+}
+
+export async function createMockStep({
+  input,
+  utensil,
+  output,
+}: SimpleStep): Promise<number> {
+  const { rows } = await query<OkPacket>(
+    "insert into step (input, utensil, output) values (:input, :utensil, :output);",
+    { input, utensil, output }
+  );
+  return rows.insertId;
 }
