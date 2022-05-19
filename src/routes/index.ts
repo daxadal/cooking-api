ce4f7cbbb071ce145dfcce6c6441eafd1b4d5425
egarciadececa <e.garciadececa@gmail.com>
2022-05-19 15:52:19 +0200

    Added try/catch for internal server error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,15 +6,24 @@ import { Ingredient, Recipe, Step, Utensil } from "@/services/db";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-  res.status(200).send({
-    environment,
-    stats: {
-      ingredients: await Ingredient.count(),
-      utensils: await Utensil.count(),
-      steps: await Step.count(),
-      recipes: await Recipe.count(),
-    },
-  });
+  const logger = res.locals.logger || console;
+  try {
+    res.status(200).send({
+      environment,
+      stats: {
+        ingredients: await Ingredient.count(),
+        utensils: await Utensil.count(),
+        steps: await Step.count(),
+        recipes: await Recipe.count(),
+      },
+    });
+  } catch (error) {
+    logger.error(
+      `Internal server error at ${req.method} ${req.originalUrl}`,
+      error
+    );
+    res.status(500).send({ message: "Internal server error" });
+  }
 });
 
 export default router;
