03470a924b7673faa388b30a9c02222c0b5999af
egarciadececa <e.garciadececa@gmail.com>
2022-06-15 16:18:00 +0200

    Delete tests modified

--- test/unit/routes/steps.test.ts
+++ test/unit/routes/steps.test.ts
@@ -243,45 +243,70 @@ describe("The /steps route", () => {
     });
   });
 
-  describe("DELETE /steps", () => {
+  describe("DELETE /steps/{input}-{utensil}-{output}", () => {
+    const INPUT_ID = 101;
+    const UTENSIL_ID = 1;
+    const OUTPUT_ID = 102;
+
     afterEach(() => clearTable("step"));
 
     it.each`
-      body                           | message                    | reason
-      ${undefined}                   | ${/is required/}           | ${"is undefined"}
-      ${{}}                          | ${/is required/}           | ${"is empty"}
-      ${{ utensil: 1, output: 102 }} | ${/"input" is required/}   | ${"has no input field"}
-      ${{ input: 101, output: 102 }} | ${/"utensil" is required/} | ${"has no utensil field"}
-      ${{ input: 101, utensil: 1 }}  | ${/"output" is required/}  | ${"has no output field"}
-    `("Returns 400 if the body $reason", async ({ body, message }) => {
+      input        | utensil       | output       | field
+      ${"INVALID"} | ${UTENSIL_ID} | ${OUTPUT_ID} | ${"input"}
+      ${INPUT_ID}  | ${"INVALID"}  | ${OUTPUT_ID} | ${"utensil"}
+      ${INPUT_ID}  | ${UTENSIL_ID} | ${"INVALID"} | ${"output"}
+    `(
+      "Returns 404 if $field is not a number",
+      async ({ input, utensil, output }) => {
+        // given
+
+        // when
+        const response = await request(app).delete(
+          `/steps/${input}-${utensil}-${output}`
+        );
+
+        // then
+        expect(response.status).toEqual(404);
+        expect(response.body).toBeDefined();
+        expect(response.body.message).toMatch("Endpoint not found");
+      }
+    );
+
+    it("Returns 404 if the step doesn't exist", async () => {
       // given
+      const input = 101;
+      const utensil = 1;
+      const output = 102;
 
       // when
-      const response = await request(app).delete("/steps").send(body);
+      const response = await request(app).delete(
+        `/steps/${input}-${utensil}-${output}`
+      );
 
       // then
-      expect(response.status).toEqual(400);
+      expect(response.status).toEqual(404);
       expect(response.body).toBeDefined();
-      expect(response.body.message).toMatch(message);
+      expect(response.body.message).toMatch("Step not found");
     });
 
     it("Returns 204 after deleting the step", async () => {
       // given
-      const body = {
-        input: 101,
-        utensil: 1,
-        output: 102,
-      };
-      await Step.create(body);
+      const input = 101;
+      const utensil = 1;
+      const output = 102;
+
+      await Step.create({ input, utensil, output });
 
       // when
-      const response = await request(app).delete("/steps").send(body);
+      const response = await request(app).delete(
+        `/steps/${input}-${utensil}-${output}`
+      );
 
       // then
       expect(response.status).toBe(204);
       expect(response.body).toStrictEqual({});
 
-      expect(await Step.get(body)).toBeUndefined();
+      expect(await Step.get({ input, utensil, output })).toBeUndefined();
     });
   });
 });
